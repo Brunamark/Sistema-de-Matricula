feb24b1ffcbc8672c3cbd706d8dff318b982ffb5
Brunamark <bruninha.markowisk@hotmail.com>
2025-08-26 12:00:19 -0300

    feat: add, edit and delete disciplina

--- code/entity/Secretario.java
+++ code/entity/Secretario.java
@@ -10,25 +10,65 @@ public class Secretario extends Usuario {
         return null;
     }
 
-    public String criarDisciplina(Disciplina disciplina) {
-        //TODO
-        return null;
+    public Codigo criarDisciplina(Disciplina disciplina) {
+        Disciplina novaDisciplina = buscarDisciplinaPorNome(disciplina.getNome());
+
+        if(novaDisciplina != null) {
+            return Codigo.ERRO_406;
+        }
+            novaDisciplina = new Disciplina(
+                disciplina.getId(),
+                disciplina.getNome(),
+                disciplina.getQuantidadeCreditos(),
+                disciplina.getCursos(),
+                disciplina.isOptativa(),
+                disciplina.isAtiva(),
+                disciplina.getAlunos(),
+                disciplina.getProfessor()
+            );
+        
+        return Codigo.OK_200;
     }
 
-    public String editarDisciplina(Disciplina disciplina) {
-        //TODO
-        return null;
+    public Codigo editarDisciplina(Disciplina disciplina) {
+        Disciplina disciplinaExistente = buscarDisciplinaPorNome(disciplina.getNome());
+        if(disciplinaExistente == null) {
+            return Codigo.ERRO_409;
+        }
+        disciplinaExistente.setNome(disciplina.getNome());
+        disciplinaExistente.setQuantidadeCreditos(disciplina.getQuantidadeCreditos());
+        disciplinaExistente.setCursos(disciplina.getCursos());
+        disciplinaExistente.setOptativa(disciplina.isOptativa());
+        disciplinaExistente.setAtiva(disciplina.isAtiva());
+        disciplinaExistente.setAlunos(disciplina.getAlunos());
+        disciplinaExistente.setProfessor(disciplina.getProfessor());
+        return Codigo.OK_200;
     }
 
     public Codigo excluirDisciplinaPorNome(String nome) {
-        //TODO
-        return null;
+        Disciplina disciplinaExistente = buscarDisciplinaPorNome(nome);
+        if(disciplinaExistente == null) {
+            return Codigo.ERRO_409;
+        }
+        return Codigo.OK_200;
     }
 
-    public String criarProfessor(Professor professor) {
-        //TODO
-        return null;
+    public Codigo criarProfessor(Professor professor) {
+        Professor novoProfessor = buscarProfessorPorNome(getNome());
+        if(novoProfessor == null){
+            return Codigo.ERRO_407;
+        }
+        novoProfessor = new Professor(
+            professor.getId(),
+            professor.getEmail(),
+            professor.getSenha(),
+            professor.getNome(),
+            professor.getDisciplina()
+        );
+        return Codigo.OK_200;
     }
+
+        
 
     public String editarProfessor(Professor professor) {
         //TODO
