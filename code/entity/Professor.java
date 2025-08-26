1d181ea4819113d70e5d5f3431e10ee289e4f14e
Brunamark <bruninha.markowisk@hotmail.com>
2025-08-26 11:59:27 -0300

    feat: construtctors, getters and setters

--- code/entity/Professor.java
+++ code/entity/Professor.java
@@ -5,6 +5,26 @@ import java.util.List;
 public class Professor extends Usuario {
     private Disciplina disciplina;
 
+    // Construtores
+    public Professor() {
+        super();
+    }
+
+    public Professor(Long id, String email, String senha, String nome, Disciplina disciplina) {
+        super(id, email, senha, nome);
+        this.disciplina = disciplina;
+    }
+
+    // Getters and Setters
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    // Métodos
     public List<Aluno> verificarAlunosMatriculadosPorDisciplina(String nomeDisciplina) {
         //TODO
         return null;
--- code/entity/Usuario.java
+++ code/entity/Usuario.java
@@ -8,13 +8,58 @@ public abstract class Usuario {
     private String senha;
     private String nome;
 
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String email, String senha, String nome) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Métodos
     public boolean efetuarLogin(String email, String senha) {
-        //TODO
+        if(this.email.equals(email) && this.senha.equals(senha)) {
+            return true;
+        }
         return false;
     }
 
     public List<Disciplina> listarDisciplinas() {
-        //TODO
+
         return null;
     }
 
